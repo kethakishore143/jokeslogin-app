925c3b3a1ff10305ae12ea4f9ce4aa1f02db98b7
kethakishore143 <kishore1993keta@gmail.com>
2024-03-14 09:28:59 +0530

    Add existing project files to Git

---
+++ src/components/Home/index.js
@@ -0,0 +1,62 @@
+import { useNavigate } from "react-router";
+import { useState, useEffect } from "react";
+import "./home.css";
+
+const Home = () => {
+  const [ApiDataJokes, setApiData] = useState([]);
+
+  const Navigate = useNavigate();
+
+  useEffect(() => {
+    getjokeData();
+  }, []);
+
+  const getjokeData = () => {
+    fetch(
+      "https://v2.jokeapi.dev/joke/any?format=json&blacklistFlags=nsfw,sexist&type=single&lang=EN&amount=10"
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setApiData(data.jokes);
+      });
+  };
+
+  const onClickLogout = () => {
+    Navigate("/login/jokeslogin-app");
+  };
+
+  const Fetchjoke = () => {
+    getjokeData();
+  };
+
+  return (
+    <div className="Home-container">
+      <div className="table-container">
+        <table>
+          <thead>
+            <tr>
+              <th className="table-heading">Jokes</th>
+            </tr>
+          </thead>
+          <tbody>
+            {ApiDataJokes.map((eachitem, index) => (
+              <tr key={index}>
+                <td className="font-style">{eachitem.joke}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      <div>
+        <button type="button" onClick={Fetchjoke} className="fetch-jokes-btn">
+          Fetch joke
+        </button>
+        <button type="button" onClick={onClickLogout} className="logout-btn">
+          Logout
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/components/Loginform/index.js
@@ -0,0 +1,90 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "./login.css";
+
+const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [usernameError, setUsernameError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+  const [credentialError, setError] = useState("");
+
+  let history = useNavigate();
+
+  const onChangeUsername = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const onChangePassword = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const OnsubmitData = (event) => {
+    event.preventDefault();
+
+    if (username === "kishore" && password === "kishore@2024") {
+      setUsernameError("");
+      setPasswordError("");
+      history("/", { replace: true });
+      console.log("Login successful");
+    } else if (username !== "kishore") {
+      setUsernameError("Enter valid username");
+      setPasswordError("");
+    } else if (password !== "kishore") {
+      setPasswordError("Enter valid password");
+      setUsernameError("");
+    } else {
+      setError("username and  password doesn't match");
+    }
+  };
+
+  return (
+    <div className="loginform-container">
+      <div>
+        <h1>login credentials</h1>
+        <p>username : kishore</p>
+        <p>password : kishore@2024</p>
+      </div>
+      <h1>Login form</h1>
+      <div className="form-conatainer">
+        <form onSubmit={OnsubmitData}>
+          <div className="inputelement-container">
+            <label htmlFor="username" className="labelElement">
+              UserName
+            </label>
+            <input
+              type="text"
+              placeholder="Enter username"
+              value={username}
+              onChange={onChangeUsername}
+              className="input-element"
+              id="username"
+            />
+            <span className="Error">{usernameError}</span>
+          </div>
+          <div className="inputelement-container">
+            <label htmlFor="password" className="labelElement">
+              Password
+            </label>
+            <input
+              type="password"
+              placeholder="Enter password"
+              id="password"
+              value={password}
+              onChange={onChangePassword}
+              className="input-element"
+            />
+            <span className="Error">{passwordError}</span>
+          </div>
+
+          <button type="submit" className="login-btn">
+            Login
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
